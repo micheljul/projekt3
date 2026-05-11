449ee6cd322765bfb2a2c403002406bf7a0e5cd2
karinabecker <becker.karina@outlook.com>
2026-05-11 12:07:46 +0200

    MK3 27: Seite zum auswählen von einloggen und registrieren erstellen

---
+++ src/app/auth/auth.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  imports: [],
+  templateUrl: './auth.html',
+  styleUrls: ['./auth.scss']
+})
+export class Auth {}
