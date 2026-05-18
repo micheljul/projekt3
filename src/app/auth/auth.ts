1c5041f038056c9efa25dba0b4780255270eb140
karinabecker <becker.karina@outlook.com>
2026-05-18 09:46:21 +0200

    .component und .scss in angular.json hinzugefügt

--- src/app/auth/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-auth',
-  imports: [],
-  templateUrl: './auth.html',
-  styleUrls: ['./auth.scss']
-})
-export class Auth {}
