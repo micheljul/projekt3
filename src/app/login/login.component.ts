6a75cb29f4ff3db2861183680e000ced6f1541e7
karinabecker <becker.karina@outlook.com>
2026-05-31 21:48:55 +0200

    MK3-51: Login mit Pflichtfeldern und Fehlermeldungen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,21 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  correctPassword = "test";
 
-  message = "";
-
-  messageColor = "black";
+  message: string = "";
+  messageColor: string = "black";
+  correctPassword: string = "test";
 
   login(firstName: string, lastName: string, password: string) {
 
-    if (password === this.correctPassword) {
-      this.message = `Login erfolgreich`;
-      this.messageColor = "green";
-    } else {
-      this.message = "Falsches Passwort";
-      this.messageColor = "red";
+    if (!firstName || !lastName) {
+
+      this.message = "Vorname und Nachname müssen ausgefüllt sein ❗";
+      this.messageColor = "orange";
+      return;
+
     }
 
+    if (password === this.correctPassword) {
+
+      this.message = `Login erfolgreich. Willkommen ${firstName} ${lastName}`;
+      this.messageColor = "green";
+
+    } else {
+
+      this.message = "Falsches Passwort";
+      this.messageColor = "red";
+
+    }
   }
 }
