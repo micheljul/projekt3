15e8eced9f67833988317b1dfef5f399fa95b144
micheljul <miceljulia3@gmail.com>
2026-06-01 12:09:37 +0200

    MK3-14 Datenbankstruktur für Schüler anlegen

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDIMP1yzTEnbdK0j0mx-OnLV0sqa13Yfk",
+  authDomain: "projekt3-8baec.firebaseapp.com",
+  projectId: "projekt3-8baec",
+  storageBucket: "projekt3-8baec.firebasestorage.app",
+  messagingSenderId: "993252235161",
+  appId: "1:993252235161:web:80f29b82943c2a21d36508",
+  measurementId: "G-0FZ4EJK78P"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ]
 };
