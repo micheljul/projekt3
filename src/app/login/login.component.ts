0c6ebb179827d4e6722f99680529796176af4594
karinabecker <becker.karina@outlook.com>
2026-06-01 11:32:10 +0200

    router-Outlet

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {RouterModule} from '@angular/router';
+import {RouterLink, RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-login',
