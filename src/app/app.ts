b78538c8713d670a06e5629dbff5f9029565ca0e
karinabecker <becker.karina@outlook.com>
2026-06-01 11:32:47 +0200

    router-Outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import {RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
