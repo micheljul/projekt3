ff65d10c95877828de15f9fdcb540a39cb80da67
karinabecker <becker.karina@outlook.com>
2026-06-08 11:45:33 +0200

    MK3-37

--- src/app/lehrer-profil/lehrer-profil.component.ts
+++ src/app/lehrer-profil/lehrer-profil.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {addDoc, collection, Firestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-lehrer-profil',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './lehrer-profil.component.html',
-  styleUrl: './lehrer-profil.component.scss',
+  styleUrl: './lehrer-profil.component.scss'
 })
-export class LehrerProfilComponent {}
+export class LehrerProfilComponent {
+
+  neuerName: string = "";
+  neuesFach: string = "";
+  neueHobbys: string = "";
+
+  private firestore: Firestore = inject(Firestore);
+
+  async datenSpeichern() {
+    const lehrerCollection = collection(this.firestore, 'lehrer_profile');
+
+    await addDoc(lehrerCollection, {
+      name: this.neuerName,
+      unterrichtsfach: this.neuesFach,
+      hobbys: this.neueHobbys,
+    });
+
+    alert('Lehrer-Profil erfolgreich gespeichert!');
+  }
+}
