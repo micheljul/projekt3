30c4889c79ed62319c9dea9cd792a572056b67c8
karinabecker <becker.karina@outlook.com>
2026-06-15 00:47:36 +0200

    MK3-58

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import {Component, signal} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,19 @@ import {HeaderComponent} from './header/header.component';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('projekt3');
+  protected readonly title = signal('projekt3')
+  showDashboardButtons = false;
+
+  constructor(private router: Router) {
+
+    this.showDashboardButtons =
+      this.router.url.includes('/dashboard');
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.showDashboardButtons =
+          this.router.url.includes('/dashboard');
+      });
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,101 @@
-import { Component } from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
+import {auth} from '../firebase.config';
+import {Router} from '@angular/router';
+import {collection, Firestore, getDocs, query, where} from '@angular/fire/firestore';
+import {signOut} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  @Input() showDashboardButtons = false;
+
+  private firestore: Firestore = inject(Firestore);
+
+  constructor(private router: Router) {
+  }
+
+  async oeffneProfil() {
+
+    const user = auth.currentUser;
+
+    if (!user) {
+      alert('Nicht eingeloggt');
+      return;
+    }
+
+    console.log('🔥 User:', user.email);
+
+    const email = user.email;
+
+    if (!email) {
+      alert('Keine Email im Login gefunden');
+      return;
+    }
+
+    let profilDaten: any = null;
+    let zielRoute = '';
+
+    // 🔍 SCHÜLER
+    const schuelerRef = collection(this.firestore, 'schueler_profile');
+    const schuelerQ = query(schuelerRef, where('email', '==', email));
+    const schuelerSnap = await getDocs(schuelerQ);
+
+    console.log('📘 Schüler gefunden:', schuelerSnap.size);
+
+    if (!schuelerSnap.empty) {
+
+      const data = schuelerSnap.docs[0];
+
+      profilDaten = {
+        id: data.id,
+        ...data.data()
+      };
+
+      zielRoute = '/schueler-detail';
+    }
+
+    // 🔍 LEHRER
+    else {
+      const lehrerRef = collection(this.firestore, 'lehrer_profile');
+      const lehrerQ = query(lehrerRef, where('email', '==', email));
+      const lehrerSnap = await getDocs(lehrerQ);
+
+      console.log('📗 Lehrer gefunden:', lehrerSnap.size);
+
+      if (!lehrerSnap.empty) {
+
+        const data = lehrerSnap.docs[0];
+
+        profilDaten = {
+          id: data.id,
+          ...data.data()
+        };
+
+        zielRoute = '/lehrer-detail';
+      }
+    }
+
+    console.log('👉 PROFIL:', profilDaten);
+
+    if (profilDaten) {
+      this.router.navigate([zielRoute], {
+        state: {
+          profil: profilDaten,
+          darfBearbeiten: true
+        }
+      });
+    } else {
+      alert('❌ Profil nicht gefunden (Email stimmt nicht mit Firestore überein)');
+    }
+  }
+
+  async logout() {
+    await signOut(auth);
+    this.router.navigate(['/login']);
+  }
+}
